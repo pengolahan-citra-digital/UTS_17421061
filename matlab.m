7184892950f27e9b795201095ba2fbedc9b5ecc1
handini <handini144@gmail.com>
2019-04-23 14:45:24 +0700

    utscitra

---
+++ matlab.m
@@ -0,0 +1,19 @@
+I = imread('D:/UTS CITRA DIGITAL/eight.tif');
+
+citra = edge(I);
+canny = edge(I,'canny');
+sobel = edge(I,'sobel');
+
+
+subplot(2,2,1);
+imshow(I);
+title('Citra asli');
+
+subplot(2,2,2);
+imshow(canny);
+title('Canny Operator');
+
+subplot(2,2,3);
+imshow(sobel);
+title('Sobel Operator');
+
